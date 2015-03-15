f3f7b58533f09abf4bacfc76fe24cb44a59ad8eb
lni_T <developer.lni@gmail.com>
2015-03-15 19:26:46 +0900

    RoomとUserをひもづけ

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315100003) do
+ActiveRecord::Schema.define(version: 20150315102159) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.string   "body",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
