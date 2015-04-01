a34a5189a275142f5ecb4e6c3cb476d824e8a47b
lni_T <developer.lni@gmail.com>
2015-04-02 02:30:10 +0900

    therubyracerを忘れてたので追加

--- script/deploy_create.sh
+++ script/deploy_create.sh
@@ -1,3 +1,4 @@
+bundle install
 bundle exec rake assets:precompile
 bundle exec rake db:setup RAILS_ENV=production
 bundle exec rake unicorn:start
--- script/deploy_update.sh
+++ script/deploy_update.sh
@@ -1,3 +1,4 @@
+bundle install
 bundle exec rake assets:precompile
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake unicorn:restart
